a13c09ccc18b63bf543f4e9f8f9f1363b5653e5f
Mond1c <kornilovichmihail@gmail.com>
2021-12-07 15:53:30 +0300

    add clear button

---
+++ src/main/java/Application/Launcher.java
@@ -0,0 +1,7 @@
+package Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.demo {
     requires javafx.fxml;
 
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    opens Application to javafx.fxml;
+    exports Application;
 }
